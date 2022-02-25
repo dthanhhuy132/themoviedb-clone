2ca5ac6d50198b7f6288d71591b1872e0b92e9ef
huydoan <dthanhhuy132@gmail.com>
2022-02-25 17:26:41 +0700

    add .env file

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseURL: 'https://api.themoviedb.org/3',
-  apiKey: 'f19c25e70b0c454b7e7f657a20399289',
+  apiKey: process.env.API_KEY,
   originalImage: (imagePath) => `https://image.tmdb.org/t/p/original${imagePath}`,
   w500Image: (imagePath) => `https://image.tmdb.org/t/p/w500${imagePath}`,
 };
